4101a5d89789958ad8f0035dcbf3de0a74bc2896
josephthomasbuckley <josephthomasbuckley@gmail.com>
2024-01-08 16:38:51 -0800

    lets go

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, redirect, session
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
+import pandas as pd 
+from rymscraper import rymscraper, RymUrl
+
 app = Flask(__name__)
 
-client_id = '5c748acdbc41475fa3cb4b63c7425139'
-client_secret = '68919f727b2244918ca9b692f4d4d0aa'
+client_id = 'c400bb430d4d498f81f6e3cdb91c4297'
+client_secret = 'd1666b600fd64f83a96bba77c063a569'
 redirect_uri = 'http://localhost:8888/callback'
 
 app.secret_key = 'random-secret-key'
@@ -19,6 +22,12 @@ sp_oauth = SpotifyOAuth(
     scope=["user-library-read", "user-read-playback-state", "user-read-currently-playing", "playlist-read-private", "user-top-read", "user-read-private"],
     cache_path=".spotify_cache",
 )
+# Scrape rym for the data:
+def rymScraper(song_list):
+    network = rymscraper.RymNetwork()
+    list_album_infos = network.get_albums_infos(names=song_list)
+    df = pd.DataFrame(list_album_infos)
+    print(df[['Name', 'RYM Rating']])
 
 @app.route('/')
 def index():
@@ -44,12 +53,16 @@ def profile():
     sp = Spotify(auth=token_info['access_token'])
     user_info = sp.current_user()
     top_artist_info = sp.current_user_top_artists(limit=20, offset=0, time_range='short_term')
-    top_song_info = sp.current_user_top_tracks(limit=50, offset=0, time_range='short_term')
-    top_song_info2 = sp.current_user_top_tracks(limit=50, offset=48, time_range='short_term')
-    output = [x['name'] for x in top_song_info2['items']]
-    output2 = [x['name'] for x in top_song_info['items']]
+    top_song_info = sp.current_user_top_tracks(limit=5, offset=0, time_range='short_term')
+    top_song_info2 = sp.current_user_top_tracks(limit=5, offset=5, time_range='short_term')
+    l = []
+    output = [ ( x['artists'][0]['name'] + (" - ") + x['album']['name']) for x in top_song_info['items']]
+    output2 = [( x['artists'][0]['name'] + (" - ") + x['album']['name']) for x in top_song_info2['items']]
     print([output, output2])
+    rymScraper(output)
     return f'Logged in as {user_info["display_name"]}'
 
 if __name__ == '__main__':
     app.run(port=8888)
+
+
